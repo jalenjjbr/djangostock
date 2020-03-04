2d5c39b920c2b8b1045b2c02022f8cfb6e107b9c
Jalen Ross <jalenjjbr@gmail.com>
2020-03-04 14:56:31 -0500

    Change event name (stock) max length to 100

--- quotes/models.py
+++ quotes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Stock(models.Model):
-    ticker = models.CharField(max_length=None)
+    ticker = models.CharField(max_length=100)
 
     def __str__(self):
         return self.ticker

c5ae67563c4248edf3b7bf8628b27eed69933b00
Jalen Ross <jalenjjbr@gmail.com>
2020-04-15 06:10:08 -0400

    migrations

--- quotes/models.py
+++ quotes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -7,10 +8,10 @@ class Stock(models.Model):
     ticker = models.TextField()
     tickerlong = models.TextField(default="default")
 
-    date = models.DateField()
-    location = models.TextField()
-    price = models.DecimalField(decimal_places=2,max_digits=10)
-    user = models.IntegerField()
+    date = models.DateField(default=date.today)
+    location = models.TextField(default="default")
+    price = models.DecimalField(decimal_places=2,max_digits=10,default=0.00)
+    user = models.IntegerField(default=0)
 
     def __str__(self):
         return self.tickerlong

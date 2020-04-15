3083856f811e7dffa61792417e0bdc6628a80566
Jalen Ross <jalenjjbr@gmail.com>
2020-04-15 05:44:37 -0400

    first attempt at adding additional fields to Stock (event) and the form on add_stock

--- quotes/models.py
+++ quotes/models.py
@@ -7,5 +7,10 @@ class Stock(models.Model):
     ticker = models.TextField()
     tickerlong = models.TextField(default="default")
 
+    date = models.DateField()
+    location = models.TextField()
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+    user = models.User()
+
     def __str__(self):
         return self.tickerlong

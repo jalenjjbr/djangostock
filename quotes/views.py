a9f343f1f1ab24e3a75508f45ebe1ba0624a9925
Jalen Ross <jalenjjbr@gmail.com>
2020-04-15 04:00:56 -0400

    attempt 2 at signup template reference

--- quotes/views.py
+++ quotes/views.py
@@ -63,4 +63,4 @@ def signup(request):
             return redirect('home')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
